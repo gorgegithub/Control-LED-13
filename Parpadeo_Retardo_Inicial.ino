a0158525ec550f8a5a525e3fe51539c3e703f9e1
gorgegithub <j.guardia.guillen@gmail.com>
2018-03-07 21:41:59 +0100

    Add files via upload

---
+++ Parpadeo_Retardo_Inicial.ino
@@ -0,0 +1,19 @@
+int led_13=13;
+bool inicio = false;
+// the setup function runs once when you press reset or power the board
+void setup() {
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(13, OUTPUT);
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  if (inicio == false){
+    delay(5000);
+    inicio = true;
+  }
+  digitalWrite(led_13, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(50);                       // wait for a second
+  digitalWrite(led_13, LOW);    // turn the LED off by making the voltage LOW
+  delay(500);                       // wait for a second
+}
---
+++ Retardo_Desde_Teclado.ino
@@ -0,0 +1,32 @@
+int led_13 = 13;
+int valor, valor_fin;
+char entrada;
+// the setup function runs once when you press reset or power the board
+void setup() {
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(led_13, OUTPUT);
+  Serial.begin(9600);
+  Serial.println("Control del tiempo mediante teclado");
+  Serial.println("Introducir el valor en milisegundos seguido de una X");
+}
+// the loop function runs over and over again forever
+void loop() {
+  if (Serial.available() > 0) {
+    entrada = Serial.read();
+    if (entrada == 'X') {
+      valor_fin = valor;
+      Serial.println(valor_fin);
+      valor = 0;
+    }
+    if (entrada >= '0' && entrada <= '9') {
+      valor = (valor * 10) + (entrada - '0');
+    }
+  }
+  if (valor_fin >= 25 && valor_fin <= 1000) {
+    digitalWrite(led_13, HIGH);   // turn the LED on (HIGH is the voltage level)
+    delay(valor_fin);
+    digitalWrite(led_13, LOW);   // turn the LED on (HIGH is the voltage level)
+    delay(valor_fin);
+
+  }
+}
