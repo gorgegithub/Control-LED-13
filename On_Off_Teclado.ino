099dbf5834a43160ea4cf6142bf44a3806bdbf48
gorgegithub <j.guardia.guillen@gmail.com>
2018-03-07 21:43:24 +0100

    Rename Parpadeo_Degradado.ino to On_Off_Teclado.ino

---
+++ On_Off_Teclado.ino
@@ -0,0 +1,21 @@
+int led_13 = 13;
+int entrada;
+// the setup function runs once when you press reset or power the board
+void setup() {
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(led_13, OUTPUT);
+  Serial.begin(9600);
+}
+// the loop function runs over and over again forever
+void loop() {
+  if (Serial.available() > 0) {
+    entrada = Serial.read();
+    if (entrada == 'H') {
+      digitalWrite(led_13, HIGH);
+    }
+    if (entrada == 'L') {
+      digitalWrite(led_13, LOW);
+    }
+  }
+
+}
